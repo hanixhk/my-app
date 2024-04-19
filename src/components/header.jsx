927d80718ac72866f0a24f39409b0bbc225ce601
hanixhk <abdulhannanxhk@gmail.com>
2024-04-20 03:03:21 +0500

    add next auth

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,14 +1,20 @@
+'use client';
+import Image from "next/image";
 import Link from "next/link";
+import { signIn ,useSession,signOut} from "next-auth/react";
+
 
 
 
 export default function Header() {
+  const {data:session} = useSession();
+  console.log(session);
   return (
     <div className="shadow-sm border-b sticky top-0 bg-white z-30 p-3">
         <div className=" flex justify-between items-center max-w-6xl mx-auto">
        {/* logo */}
         <Link href="/" className="hidden lg:inline-flex">
-          <img 
+          <Image 
           className="cursor-pointer"
           src="/ig_logo.webp"
           width={96} height={96}
@@ -17,7 +23,7 @@ export default function Header() {
         </Link>         
 
         <Link href="/" className="lg:hidden ">
-          <img 
+          <Image 
           className="cursor-pointer"
           src="/ig.webp"
           width={40} height={40}
@@ -29,8 +35,15 @@ export default function Header() {
         <input type="text" placeholder="Search" className="bg-gray-50 border border-gray-200 rounded text-sm w-full py-2 px-4 max-w-[210px]" />
 
             {/* menu items*/}
-            <button className="text-sm font-semibold text-blue-500">Log in </button>
-            </div>
+            {session ? (
+              // eslint-disable-next-line @next/next/no-img-element
+              <img src={session.user.image} alt={session.user.name} className="h-10 w-10 rounded-full cursor-pointer"
+              onClick={signOut}
+              />
+            ):(
+            <button onClick={signIn} className="text-sm font-semibold text-blue-500">Log in </button>
+          )}
+         </div>
         </div>
 
   )
